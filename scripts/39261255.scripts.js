613bd7f4f03b713fb83005e721ae82f7b0255d38
Manfred Touron <m@42.am>
2014-02-05 14:13:19 +0100

    rebuild

---
+++ scripts/39261255.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("m1ch3lBizApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("m1ch3lBizApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
