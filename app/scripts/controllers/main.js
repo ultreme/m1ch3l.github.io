2135141d3ba1c0824b54f813e1293dc9106a130a
Manfred Touron <m@42.am>
2014-02-05 11:39:55 +0100

    initialized yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('m1ch3lBizApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
